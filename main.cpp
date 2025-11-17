47fda28fe8b6df9bf544dd38c9f9e8a18eb910c9
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-17 20:14:47 -0300

    criada classe Task

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char *argv[]) {
-  std::cout << "Argumentos passados: " << argc << std::endl;
+class Task {
+private:
+  int id;
+  std::string description;
+  bool isDone;
 
+public:
+  Task(int id, std::string description) {
+    this->id = id;
+    this->description = description;
+    this->isDone = false;
+  }
+  int getId() { return this->id; }
+  std::string getDescription() { return this->description; }
+
+  bool getIsDone() { return this->isDone; }
+};
+
+int main(int argc, char *argv[]) {
   if (argc > 1) {
     std::string command = argv[1];
     std::cout << "O comando passado foi: " << command << std::endl;
@@ -11,15 +27,23 @@ int main(int argc, char *argv[]) {
     if (command == "add") {
       if (argc > 2) {
         std::string argument = argv[2];
-        std::cout << "A tarefa passada foi: " << argument << std::endl;
+
+        Task newTask(1, argument);
+
+        std::cout << "A tarefa passada foi: " << newTask.getDescription()
+                  << std::endl;
+        std::cout << "A tarefa foi salva com ID: " << newTask.getId()
+                  << std::endl;
       } else {
         std::cout
             << "Faltam argumento para o comando add, tente hello add <tarefa>"
             << std::endl;
+        return 1;
       }
     }
   } else {
     std::cout << "Faltam argumentos tente <command> <args..>" << std::endl;
+    return 1;
   }
   return 0;
 }
