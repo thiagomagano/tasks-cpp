170783410f0ba4a41cb45f4e139a70e1308c02eb
Thiago Magano <thiagomagano1993@outlook.com>
2025-11-17 21:19:54 -0300

    Importando lib pra lidar com json, criando primeiros registros no json

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include "Task.h"
+#include <fstream>
 #include <iostream>
 #include <string>
+#include <vector>
+
+// Biblioteca para lidar melhor com JSON em C++
+#include "include/nlohmann/json.hpp"
+
+using json = nlohmann::json;
 
 int main(int argc, char *argv[]) {
   if (argc > 1) {
@@ -9,13 +16,34 @@ int main(int argc, char *argv[]) {
 
     if (command == "add") {
       if (argc > 2) {
-        std::string argument = argv[2];
+        std::string description = argv[2];
+        std::vector<Task> taskList;
+        int lastId = 0;
 
-        Task newTask(1, argument);
+        taskList.push_back(Task(lastId + 1, description));
 
-        std::cout << "A tarefa passada foi: " << newTask.getDescription()
+        // Criando Json
+        json j;
+        j["tasks"] = json::array();
+
+        // Colocando cada item da task list em um formato de json
+        for (Task t : taskList) {
+          json task_json;
+          task_json["id"] = t.getId();
+          task_json["description"] = t.getDescription();
+          task_json["isDone"] = t.getIsDone();
+
+          j["tasks"].push_back(task_json);
+        }
+
+        // Salvando Arquivo
+        std::ofstream db("db.json");
+        db << j.dump(4);
+        db.close();
+
+        std::cout << "A tarefa passada foi: " << taskList[0].getDescription()
                   << std::endl;
-        std::cout << "A tarefa foi salva com ID: " << newTask.getId()
+        std::cout << "A tarefa foi salva com ID: " << taskList[0].getId()
                   << std::endl;
       } else {
         std::cout
